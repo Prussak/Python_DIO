abacf6bd9ea699a6cd53c941fafe5a15de1b0b5e
Tamara <60240971+Prussak@users.noreply.github.com>
2021-05-06 15:01:47 -0300

    Add files via upload

---
+++ primo.py
@@ -0,0 +1,13 @@
+a = int(input('Entre com um número: '))
+
+div = 0
+for x in range(1, a+1):
+    resto = a % x
+    print(x, resto)
+    if resto == 0:
+        div = div + 1
+ 
+if div == 2:
+    print('numero {} é primo.' .format(a))
+else:
+        print('o numero {} não é primo' .format(a))
